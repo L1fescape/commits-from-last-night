ce4e48ca5fbd5cbd5c108be7ef3e478b5aa95f34
Andrew Kennedy <akennedy2292@gmail.com>
2012-07-16 01:26:59 -0400

    testin shits

--- server/app.py
+++ server/app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template, request, redirect
 import smtplib
+import urllib2
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		email = request.json["email"]
-		message = request.json["message"]
-		print email
-		print message
-		return render_template('index.html')
-	else:
-		print "GET"
-		return render_template('index.html')
+  if request.method == 'POST':
+    email = request.json["email"]
+    message = request.json["message"]
+
+    url = "https://api.github.com/legacy/user/email/"+email
+    f = urllib2.urlopen(url)
+    response = f.read()
+    f.close()
+
+    print url
+    print response
+    print email
+    print message
+
+
+    return render_template('index.html')
+  else:
+    print "GET"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run("akenn.org", 9000)
+  app.run("akenn.org", 9000)
+
