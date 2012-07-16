7aede780c92f72d964dfca9f7dce74d960c95294
Andrew Kennedy <akennedy2292@gmail.com>
2012-07-16 03:02:56 -0400

    almost finished #cfln

--- server/app.py
+++ server/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect
 import smtplib
 import urllib2
+from time import gmtime, strftime
+import pymongo
+import simplejson
+from pymongo import Connection
 
+connection = Connection('localhost', 27017)
+db = connection.commits
+collection = db.commits
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,17 +21,20 @@ def index():
     f = urllib2.urlopen(url)
     response = f.read()
     f.close()
+    response = simplejson.loads(response)
 
-    print url
-    print response
-    print email
-    print message
+    realname = response['user']['name']
+    username = response['user']['login']
+    time = strftime("%d-%m-%Y %H:%M:%S")
+    picture = "http://www.gravatar.com/avatar/"+response['user']['gravatar_id']
+    commitId = message
 
+    collection.insert({ "username":username, "realname":realname, "email":email, "picture":picture, "message":message, "commitId":commitId, "time":time })
 
-    return render_template('index.html')
+    return ""
   else:
-    print "GET"
-    return render_template('index.html')
+    commits = collection.find({})
+    return render_template('index.html', commits=commits)
 
 if __name__ == '__main__':
   app.run("akenn.org", 9000)
